e04ee4519717d892bfd66502dafc9fe449493ce4
Deepanshu Gosavi <gosavideepanshu@gmail.com>
2021-01-22 13:35:43 +0530

    Backend API More Changes

--- src/Screens/main/Transaction.jsx
+++ src/Screens/main/Transaction.jsx
@@ -1,11 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./css/transaction.css";
 import { GiClothes } from "react-icons/gi";
 import { AiFillDownCircle } from "react-icons/ai";
 import { IoIosArrowUp } from "react-icons/io";
+import axios from "axios";
 
 function Transaction(props) {
+  const [transactionList, setTransactionList] = useState([]);
   const [activeDescription, setActiveDescription] = useState({});
+  useEffect(() => {
+    axios
+      .get(
+        `http://127.0.0.1:8000/get_user_transaction?userName=${localStorage.getItem(
+          "user_name"
+        )}&userPassword=${localStorage.getItem("user_hash")}`
+      )
+      .then((res) => {
+        console.log(res.data);
+        if (res.data["result"] !== null) {
+          setTransactionList(res.data["result"]["user_data"]);
+        }
+      })
+      .catch((e) => console.log(e));
+  }, []);
   const handleActiveDescription = (data) => {
     if (
       activeDescription[data] === null ||
@@ -19,58 +36,95 @@ function Transaction(props) {
       });
     }
   };
-  const arr = [1, 2, 3, 4, 5];
+
+  const handleDelete = (index) => {
+    axios
+      .post("http://127.0.0.1:8000/delete_transaction", {
+        transactionID: transactionList[index]["transaction_id"],
+      })
+      .then((res) => {
+        if (res.data["result"] === "deleted successfully") {
+          let newTransactionList = transactionList.filter((v, i) => {
+            if (index === i) {
+              return null;
+            }
+            return v;
+          });
+          setTransactionList(newTransactionList);
+        }
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
   return (
     <div className="transaction__container">
       <div className="transaction_list_container">
         <div className="transaction_heading">Transactions</div>
-        {arr.map((v, i) => {
-          console.log(activeDescription[i]);
-          return (
-            <div className="transaction_list_item">
-              <div className="transactin_list_item_row1">
-                <GiClothes size="40px" />
-                <div className="transaction_category">Clothing</div>
-                <div className="transaction_data_time">12:00AM</div>
-                <div className="transaction_item_description_btn">
-                  {activeDescription[i] === undefined ||
-                  activeDescription[i] === false ? (
-                    <AiFillDownCircle
-                      color="white"
-                      size="30px"
-                      onClick={() => handleActiveDescription(i)}
-                    />
-                  ) : (
-                    <IoIosArrowUp
-                      size="20px"
-                      color="#393E46"
-                      onClick={() => handleActiveDescription(i)}
-                    />
-                  )}
+        {transactionList.length === 0 ? (
+          <div
+            style={{
+              color: "white",
+              justifySelf: "center",
+              alignSelf: "center",
+              marginTop: "20px",
+            }}
+          >
+            -- No Transaction --
+          </div>
+        ) : (
+          transactionList.map((v, i) => {
+            console.log(activeDescription[i]);
+            return (
+              <div className="transaction_list_item">
+                <div className="transactin_list_item_row1">
+                  <GiClothes size="40px" />
+                  <div className="transaction_category">{v["type"]}</div>
+                  <div className="transaction_data_time">{v["date_time"]}</div>
+                  <div className="transaction_item_description_btn">
+                    {activeDescription[i] === undefined ||
+                    activeDescription[i] === false ? (
+                      <AiFillDownCircle
+                        color="white"
+                        size="30px"
+                        onClick={() => handleActiveDescription(i)}
+                      />
+                    ) : (
+                      <IoIosArrowUp
+                        size="20px"
+                        color="#393E46"
+                        onClick={() => handleActiveDescription(i)}
+                      />
+                    )}
+                  </div>
+                  <div className="transaction_amount">Rs. {v["amount"]}</div>
+                  <button
+                    className="transaction_item_delete_btn"
+                    onClick={() => handleDelete(i)}
+                  >
+                    DELETE
+                  </button>
                 </div>
-                <div className="transaction_amount">Rs. 1000</div>
-                <button className="transaction_item_delete_btn">DELETE</button>
+                <div
+                  className={
+                    activeDescription[i] === undefined ||
+                    activeDescription[i] === false
+                      ? "transaction_item_description_unactive"
+                      : "transaction_item_description_active"
+                  }
+                >
+                  <div className="transaction_item_description_heading">
+                    Description:
+                  </div>
+                  <div className="transaction_description">
+                    {v["description"]}
+                  </div>
+                </div>
               </div>
-              <div
-                className={
-                  activeDescription[i] === undefined ||
-                  activeDescription[i] === false
-                    ? "transaction_item_description_unactive"
-                    : "transaction_item_description_active"
-                }
-              >
-                <div className="transaction_item_description_heading">
-                  Description:
-                </div>
-                <div className="transaction_description">
-                  1-T-shirt 1-T-shirt 1-T-shirt 1-T-shirt 1-T-shirt 1-T-shirt
-                  1-T-shirt 1-T-shirt 1-T-shirt 1-T-shirt 1-T-shirt 1-T-shirt
-                  1-T-shirt 1-T-shirt 1-T-shirt 1-T-shirt 1-T-shirt
-                </div>
-              </div>
-            </div>
-          );
-        })}
+            );
+          })
+        )}
       </div>
     </div>
   );
--- src/Screens/main/User.jsx
+++ src/Screens/main/User.jsx
@@ -1,104 +1,89 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./css/user.css";
 import { MdCamera } from "react-icons/md";
+import axios from "axios";
 
 function User(props) {
-  return (
-    <div className="user__container">
-      <div className="user_main_container">
-        <div className="user_profile_container">
-          <div className="user_profile_image_card">
-            <img
-              src="https://avatars1.githubusercontent.com/u/64830917?s=460&u=52fa62a523515620af102c282b4a90d17efef298&v=4"
-              alt=""
-              className="user_profile_image"
-            />
-            <MdCamera className="user_image_camera_icon" size="40px" />
-          </div>
-          <div className="user_full_name">Deepanshu Gosavi⭐</div>
-          <div className="user_username">@deepanshugosavi</div>
-          <div className="user_familyId">FamilyID: @gosavi_0129</div>
-        </div>
-        <div className="user_details_container">
-          <div className="user_transaction_details">
-            <div className="user_transaction_number">
-              <div>Total Transaction:</div>
-              <span>#20</span>
+  const [user, setUser] = useState(null);
+  const [family, setFamily] = useState([]);
+  useEffect(() => {
+    axios
+      .get(
+        `http://127.0.0.1:8000/get_user_profile?userName=${localStorage.getItem(
+          "user_name"
+        )}&userPassword=${localStorage.getItem("user_hash")}`
+      )
+      .then((res) => {
+        console.log(res.data);
+        if (res.data["result"] !== null) {
+          setUser(res.data["result"]["user_data"]);
+          setFamily(res.data["result"]["member_data"]);
+        }
+      })
+      .catch((e) => console.log(e));
+  }, []);
+  if (user !== null)
+    return (
+      <div className="user__container">
+        <div className="user_main_container">
+          <div className="user_profile_container">
+            <div className="user_profile_image_card">
+              <img
+                src={
+                  user["image_url"] === ""
+                    ? "https://img.favpng.com/2/24/0/computer-icons-avatar-user-profile-png-favpng-HPjiNes3x112h0jw38sbfpDY9.jpg"
+                    : user["image_url"]
+                }
+                alt=""
+                className="user_profile_image"
+              />
+              <MdCamera className="user_image_camera_icon" size="40px" />
             </div>
-            <div className="user_transaction_total_amount">
-              <div>Total Transaction Amount: </div>
-              <span> Rs. 1028.00/- </span>
+            <div className="user_full_name">{user["full_name"]}⭐</div>
+            <div className="user_username">@{user["user_name"]}</div>
+            <div className="user_familyId">
+              FamilyID: @{user["user_family_name"]}
             </div>
           </div>
-          <div className="user_family_members">
-            <div className="user_family_member_heading">Family Members:</div>
-            <div className="user_family_member_cards">
-              <div className="user_family_member_card">
-                <img
-                  src="https://img.favpng.com/2/24/0/computer-icons-avatar-user-profile-png-favpng-HPjiNes3x112h0jw38sbfpDY9.jpg"
-                  alt=""
-                  className="user_family_member_image"
-                />
-                <div className="user_family_member_username">
-                  @deepanshugosavi
-                </div>
+          <div className="user_details_container">
+            <div className="user_transaction_details">
+              <div className="user_transaction_number">
+                <div>Total Transaction:</div>
+                <span>#{user["user_transaction_count"]}</span>
               </div>
-              <div className="user_family_member_card">
-                <img
-                  src="https://img.favpng.com/2/24/0/computer-icons-avatar-user-profile-png-favpng-HPjiNes3x112h0jw38sbfpDY9.jpg"
-                  alt=""
-                  className="user_family_member_image"
-                />
-                <div className="user_family_member_username">
-                  @deepanshugosavi
-                </div>
+              <div className="user_transaction_total_amount">
+                <div>Total Transaction Amount: </div>
+                <span> Rs. {user["user_transaction_total_amount"]}/- </span>
               </div>
-              <div className="user_family_member_card">
-                <img
-                  src="https://img.favpng.com/2/24/0/computer-icons-avatar-user-profile-png-favpng-HPjiNes3x112h0jw38sbfpDY9.jpg"
-                  alt=""
-                  className="user_family_member_image"
-                />
-                <div className="user_family_member_username">
-                  @deepanshugosavi
-                </div>
-              </div>
-              <div className="user_family_member_card">
-                <img
-                  src="https://avatars1.githubusercontent.com/u/64830917?s=460&u=52fa62a523515620af102c282b4a90d17efef298&v=4"
-                  alt=""
-                  className="user_family_member_image"
-                />
-                <div className="user_family_member_username">
-                  @deepanshugosavi
-                </div>
-              </div>
-              <div className="user_family_member_card">
-                <img
-                  src="https://avatars1.githubusercontent.com/u/64830917?s=460&u=52fa62a523515620af102c282b4a90d17efef298&v=4"
-                  alt=""
-                  className="user_family_member_image"
-                />
-                <div className="user_family_member_username">
-                  @deepanshugosavi
-                </div>
-              </div>
-              <div className="user_family_member_card">
-                <img
-                  src="https://avatars1.githubusercontent.com/u/64830917?s=460&u=52fa62a523515620af102c282b4a90d17efef298&v=4"
-                  alt=""
-                  className="user_family_member_image"
-                />
-                <div className="user_family_member_username">
-                  @deepanshugosavi
-                </div>
+            </div>
+            <div className="user_family_members">
+              <div className="user_family_member_heading">Family Members:</div>
+              <div className="user_family_member_cards">
+                {family.map((v, i) => {
+                  return (
+                    <div className="user_family_member_card">
+                      <img
+                        src={
+                          v["member_image"] === ""
+                            ? "https://img.favpng.com/2/24/0/computer-icons-avatar-user-profile-png-favpng-HPjiNes3x112h0jw38sbfpDY9.jpg"
+                            : v["member_image"]
+                        }
+                        alt=""
+                        className="user_family_member_image"
+                      />
+                      <div className="user_family_member_username">
+                        @{v["member_username"]}
+                      </div>
+                    </div>
+                  );
+                })}
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  return <div></div>;
 }
 
 export default User;
